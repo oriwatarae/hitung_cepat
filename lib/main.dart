7e1dba2357653edb312d676aeb3dade83fc53725
unknown <Ori007009@gmail.com>
2024-07-13 22:03:04 +0700

    Memperbarui kode pada main.dart: menambahkan penjelasan dalam bentuk komentar

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
 
+// Fungsi utama yang menjalankan aplikasi
 void main() {
   runApp(const MyApp());
 }
 
+// Widget utama aplikasi
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hitung Cepat',
+      title: 'Hitung Cepat', // Judul aplikasi
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue, // Tema aplikasi
       ),
-      home: const MyHomePage(),
+      home: const MyHomePage(), // Halaman utama aplikasi
     );
   }
 }
 
+// Halaman utama aplikasi
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
@@ -40,26 +43,26 @@ class MyHomePage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Macam-macam alat pengukur waktu'),
+        title: const Text('Macam-macam alat pengukur waktu'), // Judul halaman
       ),
       body: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // Ubah sesuai keinginan Anda
+          crossAxisCount: 2, // Jumlah kolom grid
         ),
-        itemCount: images.length,
+        itemCount: images.length, // Jumlah item dalam daftar
         itemBuilder: (BuildContext context, int index) {
           return Card(
             child: Column(
               children: [
                 Image.asset(
-                  images[index]['path']!,
-                  fit: BoxFit.cover,
-                  height: 250,
+                  images[index]['path']!, // Menampilkan gambar
+                  fit: BoxFit.cover, // Menyesuaikan gambar dengan ukuran kotak
+                  height: 160, // Tinggi gambar
                 ),
-                const SizedBox(height: 16),
+                const SizedBox(height: 12), // Memberi jarak antara gambar dan teks
                 Text(
-                  images[index]['description']!,
-                  style: Theme.of(context).textTheme.labelLarge,
+                  images[index]['description']!, // Menampilkan deskripsi gambar
+                  style: Theme.of(context).textTheme.labelLarge, // Gaya teks
                 ),
               ],
             ),
